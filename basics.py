bbee3814d5ace5bcf24a3fc9a8eaf5c4d40404c3
Nishant jain <nishant.jain2811@gmail.com>
2026-01-17 15:19:30 +0530

    Add basics.py with variables and input example

---
+++ basics.py
@@ -0,0 +1,13 @@
+# Variables and Data Types
+
+name = "Nishant"
+age = 20
+cgpa = 9.62
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+
+# Input and Output
+college = input("Enter your college name: ")
+print("College:", college)
