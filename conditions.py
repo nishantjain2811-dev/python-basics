b08e61ec52a4d70c86e5454f8a21592c92588793
Nishant jain <nishant.jain2811@gmail.com>
2026-01-17 15:41:27 +0530

    Conditional statements based example and practice

---
+++ conditions.py
@@ -0,0 +1,61 @@
+#CONDITIONAL STATEMENTS
+# 1) IF= if(condition): {always check}
+# 2)else if= elif(condition): {check only if 'if' condition is false}
+# 3)else = else: {check only if 'other condition' is false}
+# NESTING if statement
+# if(condition):
+#     if(condition2):
+#         STATEMENT1
+#     else:
+#         STATEMENT2
+# else:
+#     STATEMENT3
+
+
+
+#GRADE STUDENTS BASED ON MARKS
+# marks=int(input("marks:"))
+# if(marks >=90):
+#     print("A grade")  #tab gap '....' is called as "Indentation". 
+# elif(90>marks>=80):
+#     print("B grade")
+# elif(80>marks>=70):
+#     print("C grade")
+# elif(70> marks):
+#     print("d grade")
+#     print("fail")
+
+
+# PRACTICE QUESTION
+#WAP TO CHECK IF A NUMBER ENTERED BY THE USER IS ODD OR EVEN.
+# Num=int(input("Enter your NUMBER:"))
+# if(Num%2==0):
+#     print("Num is EVEN")
+# else:
+#     print("num is ODD")
+
+
+#WAP TO FIND THE GREATEST OF 3 NUMBERS ENTERED BY THE USER
+# A=int(input("Enter your NUMBER1:"))
+# B=int(input("Enter your NUMBER2:"))
+# C=int(input("Enter your NUMBER3:"))
+# if(A>B):
+#     if(A>C):
+#         print("A is grater")
+#     else:
+#         print("C is grater")
+# else:
+#     if(B>C):
+#         print("B is grater")
+#     else:
+#         print("C is grater")
+
+
+
+#WAP TO CHECK NUM IS DIVISIBLE BY 7 OR NOT
+# Num=int(input("Enter your NUMBER:"))
+# if(Num%7==0):
+#     print("Num is Divisible by 7")
+# else:
+#     print("Not Divisible by 7")
+
